6e4d65b4ef86793e56c5249b461aeecc2b646d0e
fadel-amar <fadel-amar@outlook.com>
2023-10-02 11:54:45 +0200

    Mise en place Adhérant

---
+++ src/Adherant.php
@@ -0,0 +1,76 @@
+<?php /** @noinspection PhpLanguageLevelInspection */
+
+namespace App;
+
+class Adherant
+{
+    private string $numeroAdherant;
+    private string  $prenom;
+    private string  $nom;
+    private string  $email;
+    private \DateTime  $dateAdhesion;
+
+
+    public function __construct(string $prenom,string $nom, string $email, ?string $dateAdhesion = null) {
+        $this->numeroAdherant = $this->genererNumero();
+        $this->prenom = $prenom;
+        $this->nom = $nom;
+        $this->email = $email;
+        if($dateAdhesion == null) {
+            $this->dateAdhesion = new \DateTime();
+        } else {
+            $this->dateAdhesion = \DateTime::createFromFormat('d/m/Y',$dateAdhesion);
+        }
+
+    }
+
+
+
+    public function genererNumero() : string
+    {
+        $chiffreAleatoire = rand(0,999999);
+        return "AD-{$chiffreAleatoire}";
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumeroAdherant(): string
+    {
+        return $this->numeroAdherant;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateAdhesion(): string
+    {
+        return $this->dateAdhesion->format('d/m/Y');
+    }
+
+
+}
--- tests/unitaires/adherent-test.php
+++ tests/unitaires/adherent-test.php
@@ -1,1 +1,14 @@
 <?php
+
+require "./vendor/autoload.php";
+
+// Test date adhésion :
+$adherant0 = new \App\Adherant('Joel', 'Lucas','ml.com', '30/09/2023');
+echo $adherant0->getDateAdhesion();
+
+
+
+// Test numéro adherant
+$adherant = new \App\Adherant('joel','Lucas','kl.com');
+echo $adherant->genererNumero();
+
