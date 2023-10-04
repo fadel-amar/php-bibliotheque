038d634124d85e4fe41ab52fce6c26d1f4785954
fadel-amar <fadel-amar@outlook.com>
2023-10-04 09:03:54 +0200

    Mise en place php unit

---
+++ test/ClasseATest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ClasseATest extends TestCase
+{
+    /**
+     * @test
+     */
+
+    public function Addtion_AvecDesNombresPositif_SommeCorrectPositive() {
+        $this->assertEquals(1,1);
+    }
+
+
+
+}
